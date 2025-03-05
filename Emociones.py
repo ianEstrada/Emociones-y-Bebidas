bb997c40a222b9c6e62444be382369ed92274e7a
Ian Estrada <ianestrada4321@gmail.com>
2025-03-04 22:05:20 -0800

    cambios

--- Emociones.py
+++ Emociones.py
@@ -1,28 +1,59 @@
+import os
+import sys
 import cv2
 from fer import FER
 
-cap = cv2.VideoCapture(0)
+# Función para obtener la ruta correcta dependiendo de si estamos empaquetados o no
+def resource_path(relative_path):
+    try:
+        # Si estamos empaquetados en un .exe, usa sys._MEIPASS
+        if getattr(sys, 'frozen', False):
+            # Cuando estamos empaquetados, los archivos se extraen en un directorio temporal
+            base_path = sys._MEIPASS
+        else:
+            # Si no estamos empaquetados, usamos la ruta relativa actual
+            base_path = os.path.abspath(".")
+        
+        return os.path.join(base_path, relative_path)
+    except Exception as e:
+        print("Error al obtener la ruta del recurso:", e)
+        return None
 
+# Rutas para el archivo XML
+xml_path = resource_path('Data Models/haarcascade_frontalface_default.xml')
+
+# Verificación de ruta del archivo XML
+if not xml_path:
+    print("Error: No se pudo encontrar el archivo XML.")
+    sys.exit(1)
+
+# Cargar el clasificador de caras
+face_cascade = cv2.CascadeClassifier(xml_path)
+
+# Crear un detector de emociones (utilizando el modelo preentrenado por defecto)
 emotion_detector = FER()
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# Inicializar la captura de video
+cap = cv2.VideoCapture(0)
 
 cv2.namedWindow("Detección de Emociones", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("Detección de Emociones", 800, 600)  
+cv2.resizeWindow("Detección de Emociones", 1280,720)
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
 
+    # Convertir la imagen a escala de grises
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # Detectar caras
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
     for (x, y, w, h) in faces:
-       
         face = frame[y:y+h, x:x+w]
 
+        # Detectar emociones en la cara detectada
         emotions, _ = emotion_detector.top_emotion(face)
 
         # Si se detecta una emoción, mostrarla en la pantalla
