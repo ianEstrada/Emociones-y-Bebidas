79db413ecc603ea40d7f03b1103dc4dbce3a06ac
Ian Estrada <ianestrada4321@gmail.com>
2025-03-05 22:03:26 -0800

    changes

--- Bebida.py
+++ Bebida.py
@@ -2,10 +2,10 @@ from ultralytics import YOLO
 import cv2
 
 # Cargar el modelo entrenado
-model = YOLO('C:\\Users\\Lightning\\Documents\\Proyecto_Python\\best_model.pt')  # Ruta de tu modelo entrenado
+model = YOLO('C:\\Users\\Lightning\\Documents\\Proyecto_Python\\best_model.pt')
 
 # Configurar la cámara
-cap = cv2.VideoCapture(0)  # 0 es el índice de la cámara por defecto
+cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
@@ -17,14 +17,11 @@ while True:
     # Realizar la inferencia
     results = model(frame)
 
-    # Los resultados devuelven una lista de imágenes con las predicciones
-    # Renderizamos los resultados sobre la imagen
     frame = results[0].plot()  # Usamos el primer (y único) resultado para plotear las predicciones
 
     # Mostrar la imagen con las predicciones
     cv2.imshow('Beverage Recognition', frame)
 
-    # Salir del bucle si se presiona la tecla 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
